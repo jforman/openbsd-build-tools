7506ae78cd7fe7b7bf902fd9f280092c84e9ba0d
jeffrey forman <jeff@jeffreyforman.net>
2015-05-30 06:34:11 -0400

    large re-rewrite of builder. now includes support for building a release

--- build-openbsd.py
+++ build-openbsd.py
@@ -1,67 +1,91 @@
 #!/usr/bin/env python
-"""Wrapper around updating, building, and installing updated
-OpenBSD source code.
-
-Provides the ability to update the installed machine's branch code,
-hopefully to STABLE.
-
-Otherwise, allows for automated following of CURRENT.
-"""
+"""Wrapper for updating, building, and creating OpenBSD releases."""
 
 import argparse
 import collections
 import datetime
 import os
 import platform
+import re
 import subprocess
 import sys
 
 BUILD_LOG = collections.OrderedDict()
-CVS_SERVER_PATH = "anoncvs@openbsd.cs.toronto.edu:/cvs"
-CVS_TAG = "/usr/src/CVS/Tag"
-PLATFORM = platform.machine()
+
 
 class BuildException(Exception):
     """Exception class to handle errors in the build proces."""
     pass
+
 
 class RunCommandError(Exception):
     """Exception class to handle errors when a shell command is run."""
     pass
 
 
-def determine_cpu_count():
+def get_kernel_name():
+    """ Return the currently-running kernel name and build number."""
+    kernel_name = run_command("/usr/bin/uname -v", return_output=True).split("#")[0]
+    return kernel_name
+
+
+def get_cpu_count():
     """ Return the number of CPUs OpenBSD is using."""
-    cpu_count = int(run_command("/sbin/sysctl -n hw.ncpu", return_output = True))
+    cpu_count = int(run_command("/sbin/sysctl -n hw.ncpu",
+                                return_output=True))
     return cpu_count
 
-def determine_running_kernel():
-    """ Return the currently-running kernel name and build number."""
-    (kernel_name, build_number) = run_command("/usr/bin/uname -v", return_output = True).split("#")
-    return kernel_name, build_number
+
+def get_running_branch():
+    """ Return currently-running branch of installed OS."""
+    branch = run_command("/usr/bin/uname -r", return_output=True)
+    if not re.match(r'\d\.\d', branch):
+        raise BuildException("Unable to determine running branch, "
+                             "expected format \d.\d: %s" % branch)
+
+    log_build_action("Branch found to be %s." % branch)
+    return branch.replace(".", "")
 
 
 def parse_args():
     """Process arguments from command line."""
-    running_kernel = determine_running_kernel()[0]
-    parser = argparse.ArgumentParser(description="Build and install OpenBSD kernel and userland.",
+    parser = argparse.ArgumentParser(description="Build and install OpenBSD kernel, userland, and releases.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--build",
                         action="append",
-                        choices=["kernel", "userland"],
-                        help="What to build: kernel and/or userland.")
-    parser.add_argument("--updatecvs",
+                        choices=["kernel", "userland", "release"],
+                        help="Which part of OpenBSD to build.")
+    parser.add_argument("--cpus",
+                        default=get_cpu_count(),
+                        help="Number of CPU threads to pass to make. Auto-detected.")
+    parser.add_argument("--cvs_tag",
+                        help="Tag to checkout/update. Example: OPENBSD_5_5",
+                        default="HEAD")
+    parser.add_argument("--cvs_server",
+                        default="openbsd.cs.toronto.edu",
+                        help="FQDN/IP address of CVS server for OpenBSD repo.")
+    parser.add_argument("--force",
                         action="store_true",
-                        help="Checkout/update the local CVS check out.")
-    parser.add_argument("--cvstag",
-                        help="Tag to checkout/update. Example: OPENBSD_5_5")
+                        help="Some steps tend to fail. Force ignores those failures. Applies to: checkflist.")
+    parser.add_argument("-i", "--interactive",
+                        action="store_true",
+                        help="If an error occurs, pause for user decision of whether to continue.")
     parser.add_argument("--kernel",
                         choices=set(["GENERIC",
                                      "GENERIC.MP",
                                      "RAMDISK",
                                      "RAMDISK_CD",
-                                     running_kernel]),
-                        default=running_kernel, help="Name of kernel to build.")
+                                     get_kernel_name()]),
+                        default=get_kernel_name(), help="Name of kernel to build.")
+    parser.add_argument("--platform",
+                        default=platform.machine(),
+                        help="Machine platform. Auto-detected. Normally you won't need to redefine this.")
+    parser.add_argument("--update_cvs",
+                        action="store_true",
+                        help="Checkout/update the local CVS checkout.")
+    parser.add_argument("--release_base",
+                        default="/usr/release",
+                        help="Base directory on local file system where a release is built.")
     args = parser.parse_args()
     return args
 
@@ -72,16 +96,28 @@ def log_build_action(action):
     print "Build Action: %s" % action
     BUILD_LOG[now] = action
 
+def get_user_choice():
+    """Process y/n input from the user."""
+    prompt = "Continue [y,n]? "
+    result = raw_input(prompt).lower()
+    if result == "y":
+        log_build_action("User choice presented. User selected true.")
+        return True
 
-def run_command(command_path, return_output = False):
+    log_build_action("User choice presented. User selected false.")
+    return False
+
+def run_command(command_path, return_output=False, force=False,
+                interactive=False):
     """Execute passed command line."""
+
     log_build_action("Running command: %s" % command_path)
     try:
         # TODO(jforman): This logic is cludgy, but seems there is no
         # subprocess method flexible enough to watch output and/or
         # return output at the same time, without buffering everything.
         if return_output:
-            return_output = subprocess.check_output(command_path, shell=True)
+            return subprocess.check_output(command_path, shell=True)
         else:
             subprocess.check_call(command_path, shell=True)
     except OSError, err:
@@ -89,16 +125,20 @@ def run_command(command_path, return_output = False):
         raise RunCommandError
     except subprocess.CalledProcessError, err:
         log_build_action("CalledProcessError in run_command: %s" % err)
-        raise RunCommandError
+        if force:
+            log_build_action("FORCE ENABLED, CONTINUING PAST ERROR.")
+            return
+        if interactive:
+            if get_user_choice():
+                return
 
-    if return_output:
-        return return_output
+        raise RunCommandError
 
 def read_cvs_tag():
     """Attempt to read the branch name from a CVS repo checkout."""
 
     try:
-        with open(CVS_TAG) as tag_fh:
+        with open("/usr/src/CVS/Tag") as tag_fh:
             cvs_tag = tag_fh.readline()[1:].strip()
     except IOError, err:
         log_build_action("Unable to read CVS tag from disk: %s" % err)
@@ -106,29 +146,29 @@ def read_cvs_tag():
 
     return cvs_tag
 
-def checkout_or_update_cvs(args):
-    """Handle either checking out or updating an already 
-    retrieved CVS repository."""
+def checkout_or_update_cvs(cvs_tag, cvs_server):
+    """Manage a CVS repository checkout."""
     cvs_tag_options = ""
+    cvs_server_path = "anoncvs@%s:/cvs" % cvs_server
 
     if not (os.access("/usr/", os.W_OK) or os.access("/usr/src", os.W_OK)):
         log_build_action("Not enough write permissions to checkout/update "
                          "local CVS checkout in /usr/src.")
         raise RunCommandError
 
-    if args.cvstag is not None:
-        cvs_tag_options = "-r%(cvs_tag)s" % { "cvs_tag" : args.cvstag }
+    if cvs_tag is not None:
+        cvs_tag_options = "-r%s" % cvs_tag
 
-    if not os.path.exists(CVS_TAG):
+    if not os.path.exists("/usr/src/CVS/Tag"):
         log_build_action("No CVS checkout found. Attempting checkout now.")
         os.chdir("/usr")
         run_command("/usr/bin/cvs -d %(cvs_server_path)s checkout "
-                    "%(cvs_tag)s -P src" % { "cvs_server_path" : CVS_SERVER_PATH,
-                                             "cvs_tag" : cvs_tag_options })
+                    "%(cvs_tag_options)s -P src" % {"cvs_server_path" : cvs_server_path,
+                                                    "cvs_tag_options" : cvs_tag_options})
         return
 
     local_cvs_tag = read_cvs_tag()
-    if args.cvstag and (local_cvs_tag != args.cvstag):
+    if cvs_tag and (local_cvs_tag != cvs_tag):
         log_build_action("Upgrading across versions via source "
                          "is not suggested.")
         log_build_action("See: http://www.openbsd.org/faq/faq5.html#BldBinary")
@@ -138,56 +178,78 @@ def checkout_or_update_cvs(args):
                      "Executing update." % {"branch" : local_cvs_tag})
     os.chdir("/usr/src/")
     run_command("/usr/bin/cvs -d %(cvs_server_path)s up "
-                "%(cvs_tag)s -Pd" % { "cvs_server_path" : CVS_SERVER_PATH,
-                                      "cvs_tag" : cvs_tag_options })
+                "%(cvs_tag)s -Pd" % {"cvs_server_path" : cvs_server_path,
+                                     "cvs_tag" : cvs_tag_options})
 
 
-def build_and_install_kernel(args, env):
-    """ Iterate through steps to build and install kernel."""
+def build_and_install_kernel(platform, kernel, cpus):
+    """Manage building and installing an OpenBSD kernel."""
     log_build_action("Building kernel %(kernel)s for "
-                     "%(platform)s" % {"kernel" : args.kernel,
-                                       "platform" : PLATFORM })
-    os.chdir("/usr/src/sys/arch/%(platform)s/conf" % {"platform" : PLATFORM })
-    run_command("/usr/sbin/config %(kernel)s" % { "kernel" : args.kernel })
+                     "%(platform)s" % {"kernel" : kernel,
+                                       "platform" : platform})
+    os.chdir("/usr/src/sys/arch/%(platform)s/conf" % {"platform" : platform})
+    run_command("/usr/sbin/config %(kernel)s" % {"kernel" : kernel})
     os.chdir("/usr/src/sys/arch/%(platform)s/"
-             "compile/%(kernel)s" % { "platform" : PLATFORM,
-                                      "kernel" : args.kernel })
-    run_command("/usr/bin/make -j%(NUM_CPUS)d clean" % env)
-    run_command("/usr/bin/make -j%(NUM_CPUS)d" % env)
-    run_command("/usr/bin/make -j%(NUM_CPUS)d install"% env)
+             "compile/%(kernel)s" % {"platform" : platform,
+                                     "kernel" : kernel})
+    run_command("/usr/bin/make -j%d clean" % cpus)
+    run_command("/usr/bin/make -j%d" % cpus)
+    run_command("/usr/bin/make -j%d install"% cpus)
     log_build_action("Kernel build complete.")
 
 
-def build_and_install_userland(env):
+def build_and_install_userland(cpus):
     """ Iterate through steps to build and install userland."""
     log_build_action("Building userland.")
     run_command("/bin/rm -rf /usr/obj/*")
     os.chdir("/usr/src")
-    run_command("/usr/bin/make -j%(NUM_CPUS)d obj" % env)
+    run_command("/usr/bin/make -j%d obj" % cpus)
     os.chdir("/usr/src/etc")
     run_command("/usr/bin/env DESTDIR=/ /usr/bin/make "
-                "-j%(NUM_CPUS)d distrib-dirs" % env)
+                "-j%d distrib-dirs" % cpus)
     os.chdir("/usr/src")
-    run_command("/usr/bin/make -j%(NUM_CPUS)d build" % env)
+    run_command("/usr/bin/make -j%d build" % cpus)
     log_build_action("Userland build complete.")
-    
+
+def build_release(release_base, force):
+    """Build release set of installable OpenBSD files."""
+    log_build_action("Setting build environment variables.")
+    os.environ['DESTDIR'] = "%s/dest" % release_base
+    os.environ['RELEASEDIR'] = "%s/release" % release_base
+
+    log_build_action("Clearing out old build and release directories.")
+    run_command("/bin/rm -rf %s" % os.environ['DESTDIR'])
+    run_command("/bin/rm -rf %s" % os.environ['RELEASEDIR'])
+    log_build_action("Creating clean build and release directories.")
+    run_command("/bin/mkdir -p %s" % os.environ['DESTDIR'])
+    run_command("/bin/mkdir -p %s" % os.environ['RELEASEDIR'])
+    log_build_action("Building release.")
+    os.chdir("/usr/src/etc")
+    run_command("/usr/bin/make release")
+    log_build_action("Verifying release.")
+    os.chdir("/usr/src/distrib/sets")
+    run_command("/bin/sh checkflist", force=force)
+    os.chdir(os.environ['RELEASEDIR'])
+    log_build_action("Generating releaese index.")
+    run_command("/bin/ls -nT > %s/index.txt" % os.environ['RELEASEDIR'])
+
 def main():
     """Build while you build, so you can secure while you're secure."""
+    os.unsetenv("DESTDIR")
+    os.unsetenv("RELEASEDIR")
     args = parse_args()
-    env = {}
-    env["NUM_CPUS"] = determine_cpu_count()
     log_build_action("Command line args: %s" % args)
-    log_build_action("Environment: %s" % env)
-
     log_build_action("Build started.")
-    
+
     try:
-        if args.updatecvs:
-            checkout_or_update_cvs(args)
+        if args.update_cvs:
+            checkout_or_update_cvs(args.cvs_tag, args.cvs_server)
         if args.build and "kernel" in args.build:
-            build_and_install_kernel(args, env)
+            build_and_install_kernel(args.platform, args.kernel, args.cpus)
         if args.build and "userland" in args.build:
-            build_and_install_userland(env)
+            build_and_install_userland(args.cpus)
+        if args.build and "release" in args.build:
+            build_release(args.release_base, args.force)
         log_build_action("Build completed.")
     except OSError, err:
         log_build_action("Exception! OSError: %s" % err)
@@ -207,4 +269,5 @@ if __name__ == "__main__":
         main()
     except BuildException:
         print "BuildException"
+        print "Environment at time of failure: %s" % os.environ
         sys.exit(1)
